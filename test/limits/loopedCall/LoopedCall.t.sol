dc27a8b8cc47a5402fafb383e87004cdac83bfc7
oba <obatirou@gmail.com>
2022-10-27 15:20:53 +0200

    Remove ffi LoopedCall test

--- test/limits/loopedCall/LoopedCall.t.sol
+++ test/limits/loopedCall/LoopedCall.t.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
@@ -9,7 +10,9 @@ import "./Looper.sol";
 import "../../Utils.sol";
 import "../../calls/CallHyvm.sol";
 
-contract LoopedCall is Test {
+import "test/calls/limitTesting/LoopedCall_hyvm.sol";
+
+contract LoopedCallTest is Test {
     address hyvm;
     Looper looper;
     address owner;
@@ -18,35 +21,15 @@ contract LoopedCall is Test {
 
     //  =====   Set up  =====
     function setUp() public {
-
-        vm.createSelectFork(
-            vm.rpcUrl('eth')
-        );
+        vm.createSelectFork(vm.rpcUrl("eth"));
         owner = address(this);
         hyvm = HuffDeployer.deploy("HyVM");
         callHyvm = new CallHyvm();
-
-        loopedCallByteCode = getLoopedCallBytecode();
-        
+        loopedCallByteCode = type(LoopedCall).creationCode;
         looper = new Looper(hyvm, loopedCallByteCode);
     }
 
-    function getLoopedCallBytecode() public returns (bytes memory bytecode) {
-        string
-            memory bashCommand = 'cast abi-encode "f(bytes)" $(solc --optimize --bin test/calls/limitTesting/LoopedCall_hyvm.sol | head -8 | tail -1)';
-
-        string[] memory inputs = new string[](3);
-        inputs[0] = "bash";
-        inputs[1] = "-c";
-        inputs[2] = bashCommand;
-        bytecode = abi.decode(vm.ffi(inputs), (bytes));
-    }
-
     function testLooper() public {
-
-        callHyvm.callHyvm(
-            hyvm,
-            abi.encodePacked(loopedCallByteCode, abi.encode(address(looper)), uint256(0))
-        );
+        callHyvm.callHyvm(hyvm, abi.encodePacked(loopedCallByteCode, abi.encode(address(looper)), uint256(0)));
     }
 }
